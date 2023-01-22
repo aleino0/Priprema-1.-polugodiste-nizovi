f40b040977edd02f875c24646f4b8f83402ca7dc
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-01-22 19:23:18 +0100

    Add files via upload

---
+++ Priprema_nizovi1/Priprema_nizovi1/Program.cs
@@ -0,0 +1,45 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_nizovi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+            int[] niz1 = new int[10];
+            int[] niz2 = new int[10];
+            int kraj1=0, kraj2=0;
+
+            do
+            {
+                
+                Console.WriteLine("Unesite broj");
+                broj = Convert.ToInt32(Console.ReadLine());
+                if(broj%2==0)
+                {
+                    niz1[kraj1]=broj;
+                    kraj1++;
+                }
+                else
+                {
+                    niz2[kraj2]=broj;
+                    kraj2++;
+                }
+            } while (broj != 0 && kraj1<10 && kraj2<10);
+
+            Console.WriteLine("Parni niz");
+            for (int i = 0; i < kraj1; i++)
+                Console.Write(niz1[i] + " ");
+            Console.WriteLine("\nNe parni niz");
+            for (int i = 0; i < kraj2; i++)
+                Console.Write(niz2[i] + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Priprema_nizovi2/Priprema_nizovi2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_nizovi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, isti=0, max;
+            int[] niz1 = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] niz2 = { 3, 2, 8, 5, 5, 7 };
+
+            do
+            {
+                if(niz1.Length<niz2.Length)
+                {
+                    max = niz1.Length;
+                }
+                else
+                {
+                    max = niz2.Length;
+                }
+                if (niz1[i] == niz2[i])
+                {
+                    isti++;
+                }
+
+                i++;
+            } while (i < max);
+
+            Console.WriteLine("Broj članova istih vrijednosti je " + isti);
+            Console.ReadKey();
+        }
+    }
+}
